bd75f6ce62345f1793f220a952787352008c3184
Vinay <rmdort@gmail.com>
2013-08-22 19:08:15 +0800

    Updated the folder structure

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,112 +1,275 @@
-module.exports = function(grunt) {
+// Generated on 2013-08-22 using generator-webapp 0.2.7
+'use strict';
+var LIVERELOAD_PORT = 35728;
+var lrSnippet = require('connect-livereload')({port: LIVERELOAD_PORT});
+var mountFolder = function (connect, dir) {
+    return connect.static(require('path').resolve(dir));
+};
 
-  grunt.initConfig({
-    pkg: grunt.file.readJSON( 'package.json' ),
+// # Globbing
+// for performance reasons we're only matching one level down:
+// 'test/spec/{,*/}*.js'
+// use this if you want to recursively match all subfolders:
+// 'test/spec/**/*.js'
 
-    watch: {
-      scss: {
-        files: ['scss/**/*.scss'],
-        tasks: 'sass'
-      },
-      html: {
-        files: ['src/**/*.hbs'],
-        tasks: 'html'
-      },
-      js: {
-        files: ['js/**/*.js'],
-        //tasks: 'js'
-      },
-      livereload: {
-        options: {
-          livereload: true
+module.exports = function (grunt) {
+    // load all grunt tasks
+    require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+    // configurable paths
+    var yeomanConfig = {
+        app: 'src',
+        dist: 'build'
+    };
+
+    grunt.initConfig({
+        yeoman: yeomanConfig,
+        watch: {
+            scss: {
+                files: ['<%= yeoman.app %>/styles/**/*.scss'],
+                tasks: 'sass'
+            },         
+            styles: {
+                files: ['<%= yeoman.app %>/styles/{,*/}*.css'],
+                tasks: ['copy:styles']
+            },
+            jade: {
+                files: ['<%= yeoman.app %>/jade/**/*.jade'],
+                tasks: ['jade']
+            },
+            livereload: {
+                options: {
+                    livereload: LIVERELOAD_PORT
+                },
+                files: [
+                    '<%= yeoman.app %>/*.html',
+                    '.tmp/styles/**/*.css',
+                    '{.tmp,<%= yeoman.app %>}/scripts/{,*/}*.js',
+                    '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+                ]
+            }
         },
-        files: [
-          '/*.html',
-          'dist/assets/css/{,*/}*.css',
-          'dist/assets/js/{,*/}*.js'
-        ]
-      }
-    },
+        sass: {
+            build: {
+                files : [
+                    {
+                        src : ['**/*.scss', '!**/_*.scss'],
+                        cwd : '<%= yeoman.app %>/styles',
+                        dest : '.tmp/styles/',
+                        ext : '.css',
+                        expand : true
+                    }
+                ],
+                options : {
+                    style : 'nested'
+                }
+            }
+        },
+        jade: {
+            compile: {
+                files: [{
+                    expand: true,
+                    cwd: '<%= yeoman.app %>/jade',
+                    dest: '<%= yeoman.app %>/',
+                    src: ['**/*.jade', '!**/_*'],
+                    ext: '.html'
+                }]                
+            },
+            options: {
+                client: false,
+                pretty: true,
+                basedir: '<%= yeoman.app %>/jade'
+            }
+        }, 
+        connect: {
+            options: {
+                port: 8000,
+                // change this to '0.0.0.0' to access the server from outside
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            lrSnippet,
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, yeomanConfig.app)
+                        ];
+                    }
+                }
+            },
+            test: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            mountFolder(connect, '.tmp'),
+                            mountFolder(connect, 'test')
+                        ];
+                    }
+                }
+            },
+            dist: {
+                options: {
+                    middleware: function (connect) {
+                        return [
+                            mountFolder(connect, yeomanConfig.dist)
+                        ];
+                    }
+                }
+            }
+        },
+        open: {
+            server: {
+                path: 'http://localhost:<%= connect.options.port %>'
+            }
+        },
+        clean: {
+            dist: {
+                files: [{
+                    dot: true,
+                    src: [                        
+                        '<%= yeoman.dist %>/*',
+                        '!<%= yeoman.dist %>/.git*'
+                    ]
+                }]
+            },
+            server: '.tmp'
+        },
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            all: [
+                'Gruntfile.js',
+                '<%= yeoman.app %>/scripts/{,*/}*.js',
+                '!<%= yeoman.app %>/scripts/vendor/*',
+                'test/spec/{,*/}*.js'
+            ]
+        },        
+        useminPrepare: {
+            options: {
+                dest: '<%= yeoman.dist %>'
+            },
+            html: ['<%= yeoman.app %>/*.html']
+        },
+        usemin: {
+            options: {
+                dirs: ['<%= yeoman.dist %>']
+            },
+            html: ['<%= yeoman.dist %>/{,*/}*.html'],
+            css: ['<%= yeoman.dist %>/styles/{,*/}*.css']
+        },
+        htmlmin: {
+            build: {
+                options: {
+                    // removeCommentsFromCDATA: true,
+                    // https://github.com/yeoman/grunt-usemin/issues/44
+                    // collapseWhitespace: true,
+                    // collapseBooleanAttributes: true,
+                    // removeAttributeQuotes: true,
+                    // removeRedundantAttributes: true,
+                    // useShortDoctype: true,
+                    // removeEmptyAttributes: true,
+                    // removeOptionalTags: true
+                },
+                files: [{
+                    expand: true,
+                    cwd: '<%= yeoman.app %>',
+                    src: '*.html',
+                    dest: '<%= yeoman.dist %>'
+                }]
+            }
+        },
+        rev: {
+            dist: {
+                files: {
+                    src: [
+                        '<%= yeoman.dist %>/scripts/{,*/}*.js',
+                        '<%= yeoman.dist %>/styles/{,*/}*.css',
+                        '<%= yeoman.dist %>/images/{,*/}*.{png,jpg,jpeg,gif,webp}',
+                        '<%= yeoman.dist %>/styles/fonts/*'
+                    ]
+                }
+            }
+        },
+        
+        copy: {
+            dist: {
+                files: [{
+                    expand: true,
+                    dot: true,
+                    cwd: '<%= yeoman.app %>',
+                    dest: '<%= yeoman.dist %>',
+                    src: [
+                        '*.{ico,png,txt}',
+                        '.htaccess',
+                        '*.html',
+                        //'scripts/*',
+                        'images/{,*/}*.{webp,gif,png}',
+                        'styles/*.css',
+                        'json/*.json'
+                    ]
+                }]
+            },
+            styles: {
+                expand: true,
+                dot: true,
+                cwd: '<%= yeoman.app %>/styles',
+                dest: '.tmp/styles/',
+                src: '{,*/}*.css'
+            }            
+        },
+        concurrent: {
+            server: [                            
+                'sass',
+                'jade'
+            ],
+            test: [
+                'coffee',
+                'copy:styles'
+            ],
+            dist: [                
+                'copy:styles',                
+                'jade',
+                'htmlmin'            
+            ]
+        }        
+    });
 
-    yuidoc: {
-      compile: {
-        name: '<%= pkg.name %>',
-        description: '<%= pkg.description %>',
-        version: '<%= pkg.version %>',
-        url: '<%= pkg.homepage %>',
-        options: {
-          paths: 'js/',
-          outdir: 'docs/'
+    grunt.registerTask('server', function (target) {
+        if (target === 'dist') {
+            return grunt.task.run(['build', 'open', 'connect:dist:keepalive']);
         }
-      }
-    },
 
-    sass: {
-      build: {
-        files : [
-          {
-            src : ['**/*.scss', '!**/_*.scss'],
-            cwd : 'scss',
-            dest : 'css',
-            ext : '.css',
-            expand : true
-          }
-        ],
-        options : {
-          style : 'expanded'
-        }
-      }
-    },
+        grunt.task.run([
+            'clean:server',            
+            'concurrent:server',            
+            'connect:livereload',
+            'open',
+            'watch'
+        ]);
+    });
 
+    grunt.registerTask('test', [
+        'clean:server',
+        'concurrent:test',        
+        'connect:test',
+        'mocha'
+    ]);
 
-    
-    connect: {
-      server: {
-        options: {
-          hostname: null,
-          port: 8000,
-          base: './'
-        }
-      }
-    },
+    grunt.registerTask('build', [
+        'clean:dist',
+        'useminPrepare',
+        'concurrent:dist',
+        'concat',
+        //'uglify',
+        'copy:dist',        
+        'usemin'
+    ]);
 
-    copy:{
-      css:{
-        files: [
-          { expand: true, cwd: './css', src: ['./**/*.*'], dest: 'dist/css' }
-        ]
-      },
-      html:{
-        files: [
-          { expand: true, cwd: './', src: ['./*.html'], dest: 'dist/'}
-        ]
-      },
-      js:{
-        files: [
-          { expand: true, cwd: './js', src: ['./**/*.*'], dest: 'dist/js' }
-        ]
-      },
-      json:{
-        files: [
-          { expand: true, cwd: './json', src: ['./*.json'], dest: 'dist/json' }
-        ]
-      },
-      img:{
-        files: [
-          { expand: true, cwd: './img', src: ['./*'], dest: 'dist/img' }
-        ]
-      }
-    }
-
-
-  });
-
-  // Default task
-  
-  grunt.registerTask('default', ['copy']);
-  grunt.registerTask('server', ['connect', 'watch']);
-  grunt.registerTask('build', ['copy']);
-  grunt.registerTask('docs', ['yuidoc']);
-  
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+    grunt.registerTask('default', [
+        'jshint',
+        'test',
+        'build'
+    ]);
 };
--- build/scripts/jquery.taxonomyBrowser.js
+++ build/scripts/jquery.taxonomyBrowser.js
@@ -595,3 +595,288 @@
 
     
 })(jQuery, window, document, undefined);
+
+(function($){
+    
+    if(!$.taxonomyBrowser){
+        $.taxonomyBrowser = new Object();
+    };
+    
+    $.taxonomyBrowser.keys = function(el, options){
+        
+        // To avoid scope issues, use 'base' instead of 'this'
+        // to reference this class from internal events and functions.
+
+        var base = this;
+        
+        // Access to jQuery and DOM versions of element
+
+        base.$el = $(el);
+
+        base.el = el;
+
+        var KEYUP = 38,
+            KEYDOWN = 40,
+            KEYLEFT = 37,
+            KEYRIGHT = 39;
+        
+        
+        // Add a reverse reference to the DOM object
+
+        base.$el.data("taxonomyBrowser.keys", base);
+
+        
+        base.init = function(){
+            
+            base.options = $.extend({},base.$el.data('taxonomyBrowser').options, options);
+            
+            /*
+              Initialize once taxonomyBrowser Root column has been added
+             */
+
+            
+            base.KeyEvents.init();            
+
+        };
+        
+        
+
+
+        /**
+        * Add events to the taxonomy browser
+        * @method initKeyEvents
+        */
+       
+        base.KeyEvents = {
+
+          init: function(){
+
+            /*
+              Reverse Lookup
+             */
+            
+            var self = this;
+            
+                        
+            /*
+              Add focus to the First Column              
+             */                        
+            
+            //base.$el.find(base.options.columnClass).eq(0).focus();
+            
+            
+            $($.fn.taxonomyBrowser.elementCache[0])
+              .find(base.options.columnClass)
+              .eq(0)
+              .focus();
+
+           
+            /**
+             * KeyDown Event Handler
+             * @param  {[type]} event             
+             */
+            
+            base.$el.on('keydown', function(event){
+
+              self.columns = base.$el.find(base.options.columnClass);
+
+              switch(event.which){
+                case KEYUP:
+                  self.moveUp.apply(self, event);
+                  break;
+
+                case KEYRIGHT:
+                  self.moveRight.apply(self);
+                  break;
+
+                case KEYDOWN:
+                  self.moveDown.apply(self);  
+                  break;
+
+                case KEYLEFT:
+                  self.moveLeft.apply(self);    
+                  break;
+
+              };
+
+
+              /*
+              Prevent Default Browser Actions
+               */
+              
+              if( event.which == KEYUP ||
+                  event.which == KEYDOWN ||
+                  event.which == KEYLEFT ||
+                  event.which == KEYRIGHT ){
+
+                event.preventDefault();
+              
+              }
+              
+              
+            });
+
+          },
+
+          moveUp: function(){            
+
+            this.move('up');
+
+          },
+
+          moveDown: function(){
+
+            
+            this.move('down');
+
+
+          },
+
+          moveRight: function(){
+
+            this.move('right');
+            
+          },
+
+          moveLeft: function(){
+
+            this.move('left');
+            
+          },
+
+          move: function(direction){
+
+
+            /*
+              Select the first item if nothing is selected
+             */
+            
+            this.$currentColumn =   this.$currentColumn? 
+                                    this.$currentColumn : 
+                                    this.columns.last().first();
+                        
+            switch(direction){
+
+              case "right":
+                this.$currentColumn = this.$currentColumn.next().length? 
+                                    this.$currentColumn.next(): 
+                                    this.columns.last();
+                break;
+
+
+              case "left":
+                this.$currentColumn = this.$currentColumn.prev(base.options.columnClass).length? 
+                                    this.$currentColumn.prev(): 
+                                    this.columns.first();
+                break;
+
+            }
+           
+
+            /*
+              Add Focus Class to the Current Column
+             */
+            
+            this.columns.removeClass('js-focus');
+
+            this.$currentColumn.addClass('js-focus');
+
+                
+            /*
+              Check if there are any active Links
+             */
+            
+            var $term = this.$currentColumn.find('li');
+
+            var $active = $term.filter('.active');
+            
+            this.$currentActive =   $active.length? 
+                                    $active.next().length? $active.next(): $term.eq(0)
+                                    : $term.eq(0);
+
+            /* Direction Up */
+                                               
+            if(direction == 'up'){
+
+              this.$currentActive =   $active.length? 
+                                      $active.prev().length? $active.prev(): $term.last()
+                                      : $term.eq(0);
+            }
+
+            /*
+              If the next active list has children: trigger click
+             */
+            
+            if(direction != 'left'){
+
+              if(this.$currentActive.hasClass('has-children')){
+                
+                /*
+                Has Children
+                 */
+                
+                if(!this.$currentActive.hasClass('active')) this.$currentActive.trigger('click');  
+
+              }else{
+
+                /*
+                No Children: 
+                1. Add Active Class
+                2. Remove the adjacent Column
+                 */
+                 
+                $term.removeClass('active');
+                
+                this.$currentActive.addClass('active');
+
+                /*
+                Remove Adjacent Column if the item has no Children
+                 */
+
+                $.fn.taxonomyBrowser.removeColumns.call(base, this.$currentColumn.data('depth') + 1);
+
+              }
+
+              /*
+                Add Focus to the Link: So the container scrolls
+              */
+
+              this.$currentActive.find('a').focus();
+
+            }
+
+
+          }
+
+        }
+        
+        // Run initializer
+
+        base.init();
+        
+    };
+
+    /* Loop Through All Elements to Instantiate the plugin */
+    
+    $.fn.taxonomybrowser_keys = function(options){
+    		
+        var elements = $.fn.taxonomyBrowser.elementCache;            		
+
+    		$.each(elements, function(index, ele){
+    			(new $.taxonomyBrowser.keys(ele, options));
+    		});
+
+    };
+
+
+    
+    // Initialize
+
+    $(window).load(function(){
+
+    	$(document).taxonomybrowser_keys();
+
+    });
+    
+    
+})(jQuery);
